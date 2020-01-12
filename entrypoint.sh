3fdd0efc88426d29c022b65f35084b1e1a5cb583
Mathieu Muller <mathieu.muller1006@gmail.com>
2020-01-12 21:40:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,6 +39,7 @@ GITHUB_REPOSITORY_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}"
 pull_request=$(jq --raw-output .pull_request "$GITHUB_EVENT_PATH")
 author=$(jq --raw-output .pull_request.user.login "$GITHUB_EVENT_PATH")
 state=$(jq --raw-output .pull_request.state "$GITHUB_EVENT_PATH")
+number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 label=$(jq --raw-output .label.name "$GITHUB_EVENT_PATH")
 echo $state
