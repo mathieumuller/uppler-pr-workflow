73a8ce1f3e8fd3c06c851c80cdf241cf31ed8e3a
Mathieu Muller <mathieu.muller1006@gmail.com>
2020-01-12 17:26:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,9 @@ GITHUB_REPOSITORY_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}"
 
 event=$(jq --compact-output . "$GITHUB_EVENT_PATH")
 
-pull_request=$event | jq --compact-output '.pull_request'
+pull_request=$($event | jq --compact-output '.pull_request')
+echo pull_request
+exit 0
 action=$event| jq --compact-output '.action'
 label=$event | jq --compact-output '.label.name'
 state=$pull_request | jq --compact-output '.state'
