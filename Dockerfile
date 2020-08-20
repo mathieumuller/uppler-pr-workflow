d3469182ba9edac7fd6c53fb85c3d7fd9e026a08
Mathieu Muller <mathieu.muller1006@gmail.com>
2020-08-20 20:18:46 +0000

    test

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM debian:9.6-slim
 
-LABEL "com.github.actions.name"="Uppler PR workflow"
-LABEL "com.github.actions.description"="Define the Uppler specific pull request workflow"
+LABEL "com.github.actions.name"="Uppler CI"
+LABEL "com.github.actions.description"="Uppler continuous integration workflow workflow"
 LABEL "com.github.actions.icon"="arrow-up-right"
 LABEL "com.github.actions.color"="gray-dark"
 LABEL version="1.0.0"
--- entrypoint.sh
+++ entrypoint.sh
@@ -32,10 +32,12 @@ if [[ -z "$FINAL_REVIEWER" ]]; then
   exit 1
 fi
 
+# GITHUB API REQUIREMENTS
 GITHUB_API_HEADER="Accept: application/vnd.github.v3+json; application/vnd.github.antiope-preview+json"
 GITHUB_AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 GITHUB_REPOSITORY_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}"
 
+# GET VARIABLES
 pull_request=$(jq --raw-output .pull_request "$GITHUB_EVENT_PATH")
 author=$(jq --raw-output .pull_request.user.login "$GITHUB_EVENT_PATH")
 state=$(jq --raw-output .pull_request.state "$GITHUB_EVENT_PATH")
@@ -43,39 +45,42 @@ number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 label=$(jq --raw-output .label.name "$GITHUB_EVENT_PATH")
 
-add_reviewers() {
-  if [[ ! -z "$1" ]]; then
-    curl -sSL \
-      -H "Content-Type: application/json" \
-      -H "${GITHUB_AUTH_HEADER}" \
-      -H "${GITHUB_API_HEADER}" \
-      -X "POST" \
-      -d "{\"reviewers\":[$1]}" \
-      "${GITHUB_REPOSITORY_URL}/pulls/${number}/requested_reviewers"
-  fi
-}
+cat pull_request
 
-if [[ $action == "labeled" ]];then
-    reviewers=""
-    if [[ "$label" == "RFR" ]]; then
-      # add permanent reviewer
-      reviewers+="\"${PERMANENT_REVIEWER}\""
-      # add random reviewer
-      available=("${AVAILABLE_REVIEWERS}") 
+# THIS FUNCTION ADD RANDOM REVIEWERS TO THE PULL REQUEST
+# add_reviewers() {
+#   if [[ ! -z "$1" ]]; then
+#     curl -sSL \
+#       -H "Content-Type: application/json" \
+#       -H "${GITHUB_AUTH_HEADER}" \
+#       -H "${GITHUB_API_HEADER}" \
+#       -X "POST" \
+#       -d "{\"reviewers\":[$1]}" \
+#       "${GITHUB_REPOSITORY_URL}/pulls/${number}/requested_reviewers"
+#   fi
+# }
+
+# if [[ $action == "labeled" ]];then
+#     reviewers=""
+#     if [[ "$label" == "RFR" ]]; then
+#       # add permanent reviewer
+#       reviewers+="\"${PERMANENT_REVIEWER}\""
+#       # add random reviewer
+#       available=("${AVAILABLE_REVIEWERS}") 
  
-      # remove author from available reviewers
-      rmv=("${author} ${PERMANENT_REVIEWER}")   
-      available=${available[@]/$rmv}    
-      # remove permanent reviewer from available reviewers
-      available=${available[@]/$PERMANENT_REVIEWER}
-      count=${#available[@]}
-      random_reviewer=${available[RANDOM%${count}]}
-      reviewers+=",\"${random_reviewer}\""
+#       # remove author from available reviewers
+#       rmv=("${author} ${PERMANENT_REVIEWER}")   
+#       available=${available[@]/$rmv}    
+#       # remove permanent reviewer from available reviewers
+#       available=${available[@]/$PERMANENT_REVIEWER}
+#       count=${#available[@]}
+#       random_reviewer=${available[RANDOM%${count}]}
+#       reviewers+=",\"${random_reviewer}\""
 
-    elif [[ "$label" == "RTM" ]]; then
-      reviewers+="\"${FINAL_REVIEWER}\""
-    fi
-    echo $reviewers
-    add_reviewers $reviewers
-fi
+#     elif [[ "$label" == "RTM" ]]; then
+#       reviewers+="\"${FINAL_REVIEWER}\""
+#     fi
+#     echo $reviewers
+#     add_reviewers $reviewers
+# fi
 
